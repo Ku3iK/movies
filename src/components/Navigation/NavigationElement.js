b38b20a627d7d91757c4a520b3d539cd4943d232
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2021-04-27 00:04:12 +0200

    navigation element content created

--- src/components/Navigation/NavigationElement.js
+++ src/components/Navigation/NavigationElement.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const NavigationElement = () => {
+const NavigationElement = props => {
     return(
         <>
+            <NavLink to={props.to}>
+                <p>{props.title}</p>
+            </NavLink>
         </>
     );
 }
