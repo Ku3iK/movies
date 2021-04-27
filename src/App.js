4d4248056131f430d8e5a76fb2f8e62b9d9f86e6
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2021-04-27 20:30:25 +0200

    data from data base fetched and ract context used

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import theme from './themes/theme';
 import GlobalStyle from './themes/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
+import Navigation from './components/Navigation/Navigation';
+import MoviesLibrary from './components/MoviesLibrary/MoviesLibrary';
+import MovieContext from './Contexts/MovieContext';
 
-function App() {
+const App = () => {
+  const [moviesList, setMoviesList] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_TOKEN}`)
+      .then(response => {
+        setMoviesList(response.data.results)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
   return (
-    <ThemeProvider theme={theme}>
-        <GlobalStyle />
-    </ThemeProvider>
+    <MovieContext.Provider value={moviesList}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <GlobalStyle />
+          <Navigation />
+          <main>
+            <Switch>
+              <Route path='/' exact component={MoviesLibrary} />  
+            </Switch>          
+          </main>
+        </BrowserRouter>
+      </ThemeProvider>
+    </MovieContext.Provider>
   );
 }
 
