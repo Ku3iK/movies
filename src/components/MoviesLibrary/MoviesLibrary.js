dbcbf700a8c5344e20b17995609847c0d76447b1
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2021-04-27 20:32:04 +0200

    movies library styled

--- src/components/MoviesLibrary/MoviesLibrary.js
+++ src/components/MoviesLibrary/MoviesLibrary.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import MovieContext from '../../Contexts/MovieContext';
+import Movie from './Movie'
 
 const MoviesLibrary = () => {
+
     return(
-        <>
-        </>
+        <MovieContext.Consumer>
+            {value => {
+                return(
+                    <>
+                        <h2>Most popular movies</h2>
+                        <MoviesContainer>
+                            {value.map(el => {
+                                return <Movie key={el.id} id={el.id} title={el.title} background={el.backdrop_path} voteAverage={el.vote_average}/>
+                            })}
+                        </MoviesContainer>
+                    </>
+                );
+            }}
+        </MovieContext.Consumer> 
     );
 }
 
-export default MoviesLibrary;
+export default MoviesLibrary;export default MoviesLibrary;
+
+const MoviesContainer = styled.div`
