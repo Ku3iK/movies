0d25572b7c98bd96b9c0c8ca76e5a7ad7150d976
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2021-04-30 12:52:03 +0200

    movie details styles updated

--- src/components/MoviesLibrary/MovieDetails.js
+++ src/components/MoviesLibrary/MovieDetails.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const MovieDetails = ({ id, title, img, voteAverage, aboutMovie, relaseDate, handleHideMovie }) => {
+const MovieDetails = ({ id, title, img, voteAverage, aboutMovie, releaseDate, originalLanguage, handleHideMovie }) => {
     const libraryOfImgUrl = "https://image.tmdb.org/t/p/w500/";
 
     return(
@@ -14,6 +14,8 @@ const MovieDetails = ({ id, title, img, voteAverage, aboutMovie, relaseDate, han
                 </div>
                 <div>
                     <h3>{title}</h3>
+                    <p>Relase date: {releaseDate}</p>
+                    <p>Original language: {originalLanguage}</p>
                     <p>IMBd: <span>{voteAverage}  <FontAwesomeIcon icon='star'/></span></p>
                     <p>{aboutMovie}</p>
                 </div>
@@ -29,10 +31,12 @@ const SingleMovieDetail = styled.div`
     flex-direction: column;
     padding: 0 2rem 2rem;
     margin: 2rem;
+    max-height: 90vh;
     background-color: ${({ theme }) => theme.color.primary};
     border: .2rem solid ${({ theme }) => theme.color.secondary};
     border-radius: 2.5rem;
     color: ${({ theme }) => theme.color.white};
+    overflow-y: auto;
     @media(min-width: ${({ theme }) => theme.breakpoints.tablets}) {
         flex-direction: row;
         width: 80%;
@@ -88,6 +92,11 @@ const SingleMovieDetail = styled.div`
         font-size: ${({ theme }) => theme.font.xs};
         @media(min-width: ${({ theme }) => theme.breakpoints.smallDesktop}) {
             font-size: ${({ theme }) => theme.font.s};
+        }
+        :nth-of-type(1),
+        :nth-of-type(2) {
+            font-size: ${({ theme }) => theme.font.xs};
+            padding: .5rem 0;
         }
     }
 
