4dd07df11ba1357c5d62feea7b8d48c16d6a0537
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2021-04-30 12:51:30 +0200

    semicolons added

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ const App = () => {
   useEffect(() => {
     axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_TOKEN}`)
       .then(response => {
-        setMoviesList(response.data.results)
+        setMoviesList(response.data.results);
       })
       .catch(err => {
         console.log(err);
       })
-  }, [])
+  }, []);
 
   return (
     <MovieContext.Provider value={moviesList}>
